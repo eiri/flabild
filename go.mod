fef8d87c5a967db345063649c8dfb44e490a2ea2
Eric Avdey <eiri@eiri.ca>
2021-06-25 17:11:24 -0300

    A little bit less basic implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eiri/flabild
 
 go 1.15
+
+require github.com/mroth/weightedrand v0.4.1
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 	"os"
 	"time"
+
+	wr "github.com/mroth/weightedrand"
 )
 
 const (
@@ -19,13 +21,27 @@ func init() {
 }
 
 func main() {
-	var alphabet = []rune("abcdefghijklmnopqrstuvwxyz")
+	choices := makeChoices()
+	chooser, _ := wr.NewChooser(choices...)
 
 	n := MIN_WORD_LEN + rand.Intn(MAX_WORD_LEN-MIN_WORD_LEN+1)
 	s := make([]rune, n)
 	for i := range s {
-		s[i] = alphabet[rand.Intn(len(alphabet))]
+		s[i] = chooser.Pick().(rune)
 	}
 
 	log.Println(string(s))
+}
+
+func makeChoices() []wr.Choice {
+	// http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+	var alphabet = []rune("abcdefghijklmnopqrstuvwxyz")
+	var weights = []uint{14810, 2715, 4943, 7874, 21912, 4200, 3693, 10795, 13318, 188, 1257, 7253, 4761, 12666, 14003, 3316, 205, 10977, 11450, 16587, 5246, 2019, 3819, 315, 3853, 128}
+
+	c := make([]wr.Choice, len(alphabet))
+	for i := range c {
+		c[i] = wr.Choice{Item: alphabet[i], Weight: weights[i]}
+	}
+
+	return c
 }
