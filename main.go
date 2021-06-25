b1171a756f9f0b41b6c669e1d90d30a72e1340c2
Eric Avdey <eiri@eiri.ca>
2021-06-25 16:56:16 -0300

    Your very basic implementation

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "log"
+import (
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const (
+	MIN_WORD_LEN = 4
+	MAX_WORD_LEN = 12
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	log.SetFlags(0)
+	log.SetOutput(os.Stdout)
+}
 
 func main() {
-	log.SetFlags(0)
+	var alphabet = []rune("abcdefghijklmnopqrstuvwxyz")
 
-	log.Println("ohai!")
+	n := MIN_WORD_LEN + rand.Intn(MAX_WORD_LEN-MIN_WORD_LEN+1)
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+
+	log.Println(string(s))
 }
