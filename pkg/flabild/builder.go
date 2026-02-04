e748d94d512f68e5d8b99c7f7fc1269056752271
Eric Avdey <eiri@eiri.ca>
2026-02-04 18:37:36 -0400

    test: add tests for flabild loader and builder

--- pkg/flabild/builder.go
+++ pkg/flabild/builder.go
@@ -12,13 +12,21 @@ const PLUGIN_API_FUNC = "BuildChoicesMap"
 
 type Pair [2]rune
 
+func NewPair(a, b rune) Pair {
+	return Pair{a, b}
+}
+
 type PairMap map[Pair][]weightedrand.Choice[rune, int]
+
+type flabildPlugin interface {
+	Lookup(string) (plugin.Symbol, error)
+}
 
 type Chooser struct {
 	choices PairMap
 }
 
-func NewChooser(p *plugin.Plugin) (*Chooser, error) {
+func NewChooser(p flabildPlugin) (*Chooser, error) {
 	f, err := p.Lookup(PLUGIN_API_FUNC)
 	if err != nil {
 		return nil, fmt.Errorf("can't load plugin API: %w", err)
